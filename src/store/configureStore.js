977f0e072f89fd61f86472f34f21acd96b4d9691
mowanping <mowanping@howzhi.com>
2017-04-07 17:50:14 +0800

    readme

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,16 +2,17 @@ import { createStore, applyMiddleware } from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import invariant from 'redux-immutable-state-invariant'
 import thunk from 'redux-thunk'
-import reducer from '../reducers'
+import rootReducer from '../reducers'
 
 //applyMiddleware来自redux可以包装store的dispatch
 
 //thunk作用是使action创建函数可以返回一个function代替一个action对象
 
-const configureStore = (preloadedState) =>{
-	let store = createStore(reducer,preloadedState,composeWithDevTools(
-		applyMiddleware(invariant(), thunk)
-	));
+const configureStore = () =>{
+	let store = createStore(
+		rootReducer,
+		applyMiddleware(thunk)
+	);
 	//热替换选项
 	if(module.hot){
 		module.hot.accept('../reducers',()=>{
